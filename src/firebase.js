7279ddabc7292fdecbf269c50ec7370bb631c0da
yogesh mishra <yogeshmishra667@gmail.com>
2023-02-04 14:53:44 +0530

    environment variable

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCXsYAFiGyIyl9VyOHV8edhkZBcYJjWjfo',
-  authDomain: 'resolute-ai-40bd8.firebaseapp.com',
-  projectId: 'resolute-ai-40bd8',
-  storageBucket: 'resolute-ai-40bd8.appspot.com',
-  messagingSenderId: '326129261700',
-  appId: '1:326129261700:web:5d81f4dcc14d65a2c8f6d1',
-  measurementId: 'G-30WLYM07PJ',
+  apiKey: process.env.REACT_APP_FIREBASE_API,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MESUREMENT_ID,
 };
 
 // Initialize Firebase
